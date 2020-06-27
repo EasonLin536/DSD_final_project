801306a8b9153ed0bdac063d13629d05a5a1f707
chris94137 <chris100253@gmail.com>
2020-06-27 23:24:50 +0800

    new L2

--- src_extension/cache_2way_L2.v
+++ src_extension/cache_2way_L2.v
@@ -1,4 +1,4 @@
-`define L2_INDEX_LEN 5
+`define L2_INDEX_LEN 4
 `define L2_TAG_LEN   28 - `L2_INDEX_LEN
 `define L2_BLOCK_NUM 2 ** `L2_INDEX_LEN
 `define L2_DATA_LEN  130 + `L2_TAG_LEN
@@ -45,7 +45,7 @@ module cache(
     reg     [31:0] proc_rdata;
     reg            proc_stall;
     // state, cache
-    reg    [155:0] cache_dm[0:3][0:1], next_cache_dm[0:3][0:1];
+    reg    [155:0] cache_2way[0:3][0:1], next_cache_2way[0:3][0:1];
     reg      [7:0] LRU, next_LRU;
     reg      [2:0] state, next_state;
     // proc addr
@@ -88,33 +88,33 @@ module cache(
     // io proc
     // io mem
     // proc addr
-    assign index = proc_addr[3:2];
-    assign tag = proc_addr[29:4];
+    assign index        = proc_addr[3:2];
+    assign tag          = proc_addr[29:4];
     // data choose
-    assign dirty[0] = cache_dm[index][0][155];
-    assign valid[0] = cache_dm[index][0][154];
-    assign cache_tag[0] = cache_dm[index][0][153:128];
-    assign dirty[1] = cache_dm[index][1][155];
-    assign valid[1] = cache_dm[index][1][154];
-    assign cache_tag[1] = cache_dm[index][1][153:128];
+    assign dirty[0]     = cache_2way[index][0][155];
+    assign valid[0]     = cache_2way[index][0][154];
+    assign cache_tag[0] = cache_2way[index][0][153:128];
+    assign dirty[1]     = cache_2way[index][1][155];
+    assign valid[1]     = cache_2way[index][1][154];
+    assign cache_tag[1] = cache_2way[index][1][153:128];
     // COMPARE
-    assign hit[0] = (tag == cache_tag[0]);
-    assign hit[1] = (tag == cache_tag[1]);
-    assign word_idx = proc_addr[1:0];
+    assign hit[0]       = (tag == cache_tag[0]);
+    assign hit[1]       = (tag == cache_tag[1]);
+    assign word_idx     = proc_addr[1:0];
     
     integer i;
     always @(*) begin
         for(i = 0; i < 4; i = i + 1) begin
-            next_cache_dm[i][0] = cache_dm[i][0];
-            next_cache_dm[i][1] = cache_dm[i][1];
+            next_cache_2way[i][0] = cache_2way[i][0];
+            next_cache_2way[i][1] = cache_2way[i][1];
         end
-        next_LRU = LRU;
-        next_state = state;
-        proc_stall = 0;
-        proc_rdata = 0;
-        next_L1_read = L1_read;
+        next_LRU      = LRU;
+        next_state    = state;
+        proc_stall    = 0;
+        proc_rdata    = 0;
+        next_L1_read  = L1_read;
         next_L1_write = L1_write;
-        next_L1_addr = L1_addr;
+        next_L1_addr  = L1_addr;
         next_L1_wdata = L1_wdata;
         case(state)
             IDLE: begin
@@ -133,37 +133,37 @@ module cache(
                     if(proc_read) begin
                         case(word_idx)
                             2'b00: begin
-                                proc_rdata = cache_dm[index][0][31:0];
+                                proc_rdata = cache_2way[index][0][31:0];
                             end
                             2'b01: begin
-                                proc_rdata = cache_dm[index][0][63:32];
+                                proc_rdata = cache_2way[index][0][63:32];
                             end
                             2'b10: begin
-                                proc_rdata = cache_dm[index][0][95:64];
+                                proc_rdata = cache_2way[index][0][95:64];
                             end
                             2'b11: begin
-                                proc_rdata = cache_dm[index][0][127:96];
+                                proc_rdata = cache_2way[index][0][127:96];
                             end
                             default: proc_rdata = 0;
                         endcase
                     end
                     else if(proc_write) begin
-                        next_cache_dm[index][0][155] = 1; // set dirty
+                        next_cache_2way[index][0][155] = 1; // set dirty
                         case(word_idx)
                             2'b00: begin
-                                next_cache_dm[index][0][31:0] = proc_wdata;
+                                next_cache_2way[index][0][31:0] = proc_wdata;
                             end
                             2'b01: begin
-                                next_cache_dm[index][0][63:32] = proc_wdata;
+                                next_cache_2way[index][0][63:32] = proc_wdata;
                             end
                             2'b10: begin
-                                next_cache_dm[index][0][95:64] = proc_wdata;
+                                next_cache_2way[index][0][95:64] = proc_wdata;
                             end
                             2'b11: begin
-                                next_cache_dm[index][0][127:96] = proc_wdata;
+                                next_cache_2way[index][0][127:96] = proc_wdata;
                             end
                             default: begin
-                                next_cache_dm[index][0] = 0;
+                                next_cache_2way[index][0] = 0;
                             end
                         endcase
                     end
@@ -174,37 +174,37 @@ module cache(
                     if(proc_read) begin
                         case(word_idx)
                             2'b00: begin
-                                proc_rdata = cache_dm[index][1][31:0];
+                                proc_rdata = cache_2way[index][1][31:0];
                             end
                             2'b01: begin
-                                proc_rdata = cache_dm[index][1][63:32];
+                                proc_rdata = cache_2way[index][1][63:32];
                             end
                             2'b10: begin
-                                proc_rdata = cache_dm[index][1][95:64];
+                                proc_rdata = cache_2way[index][1][95:64];
                             end
                             2'b11: begin
-                                proc_rdata = cache_dm[index][1][127:96];
+                                proc_rdata = cache_2way[index][1][127:96];
                             end
                             default: proc_rdata = 0;
                         endcase
                     end
                     else if(proc_write) begin
-                        next_cache_dm[index][1][155] = 1; // set dirty
+                        next_cache_2way[index][1][155] = 1; // set dirty
                         case(word_idx)
                             2'b00: begin
-                                next_cache_dm[index][1][31:0] = proc_wdata;
+                                next_cache_2way[index][1][31:0] = proc_wdata;
                             end
                             2'b01: begin
-                                next_cache_dm[index][1][63:32] = proc_wdata;
+                                next_cache_2way[index][1][63:32] = proc_wdata;
                             end
                             2'b10: begin
-                                next_cache_dm[index][1][95:64] = proc_wdata;
+                                next_cache_2way[index][1][95:64] = proc_wdata;
                             end
                             2'b11: begin
-                                next_cache_dm[index][1][127:96] = proc_wdata;
+                                next_cache_2way[index][1][127:96] = proc_wdata;
                             end
                             default: begin
-                                next_cache_dm[index][1] = 0;
+                                next_cache_2way[index][1] = 0;
                             end
                         endcase
                     end
@@ -213,7 +213,7 @@ module cache(
                     next_state = WB;
                     proc_stall = 1;
                     next_L1_write = 1;
-                    next_L1_wdata = cache_dm[index][LRU[index]];
+                    next_L1_wdata = cache_2way[index][LRU[index]];
                     next_L1_addr = {cache_tag[LRU[index]], index};
                 end
                 else begin // cache miss && (block clean or invalid)
@@ -231,10 +231,10 @@ module cache(
                     else
                         proc_stall = 0;
                     next_L1_read = 0;
-                    next_cache_dm[index][LRU[index]][155] = 1'b0; // set dirty
-                    next_cache_dm[index][LRU[index]][154] = 1'b1; // set valid
-                    next_cache_dm[index][LRU[index]][153:128] = tag; // set tag
-                    next_cache_dm[index][LRU[index]][127:0] = L1_rdata;
+                    next_cache_2way[index][LRU[index]][155] = 1'b0; // set dirty
+                    next_cache_2way[index][LRU[index]][154] = 1'b1; // set valid
+                    next_cache_2way[index][LRU[index]][153:128] = tag; // set tag
+                    next_cache_2way[index][LRU[index]][127:0] = L1_rdata;
                     case(word_idx)
                         2'b00: begin
                             proc_rdata = L1_rdata[31:0];
@@ -264,6 +264,7 @@ module cache(
                     next_L1_write = 0;
                     next_L1_addr = proc_addr[29:2];
                     next_state = ALLOCATE;
+                    next_L1_read = 1;
                 end
                 else begin
                     next_L1_write = 1;
@@ -280,8 +281,8 @@ module cache(
     always @( posedge clk ) begin
         if( proc_reset ) begin
             for(j = 0; j < 4; j = j + 1) begin
-                cache_dm[j][0] <= 0;
-                cache_dm[j][1] <= 0;
+                cache_2way[j][0] <= 0;
+                cache_2way[j][1] <= 0;
             end
             LRU <= 0;
             state <= IDLE;
@@ -293,8 +294,8 @@ module cache(
         end
         else begin
             for(j = 0; j < 4; j = j + 1) begin 
-                cache_dm[j][0] <= next_cache_dm[j][0];
-                cache_dm[j][1] <= next_cache_dm[j][1];
+                cache_2way[j][0] <= next_cache_2way[j][0];
+                cache_2way[j][1] <= next_cache_2way[j][1];
             end
             LRU <= next_LRU;
             state <= next_state;
@@ -349,8 +350,8 @@ module L2(
     reg              [127:0] proc_rdata;
     reg                      proc_stall;
     // io mem
-    reg                      data_ready;
-    reg                      mem_read, mem_write;
+    reg                      mem_read, next_mem_read;
+    reg                      mem_write, next_mem_write;
     reg               [27:0] mem_addr, next_mem_addr;
     reg              [127:0] mem_wdata, next_mem_wdata;
     // state, cache
@@ -385,8 +386,8 @@ module L2(
         next_state     = state;
         proc_stall     = 0;
         proc_rdata     = cache_dm[index];
-        mem_read       = 0;
-        mem_write      = 0;
+        next_mem_read = mem_read;
+        next_mem_write = mem_write;
         next_mem_addr  = mem_addr;
         next_mem_wdata = mem_wdata;
         case(state)
@@ -412,26 +413,27 @@ module L2(
                 else if (valid && dirty) begin // cache miss && (block valid && dirty)
                     next_state     = WB;
                     proc_stall     = 1;
-                    mem_write      = 1;
+                    next_mem_write      = 1;
                     next_mem_wdata = cache_dm[index];
                     next_mem_addr  = { cache_tag, index };
                 end
                 else if (proc_read | proc_write) begin // cache miss && (block clean or invalid)
                     next_state    = ALLOCATE;
                     proc_stall    = 1;
-                    mem_read      = 1;
+                    next_mem_read = 1;
                     next_mem_addr = proc_addr;
                 end
             end
             ALLOCATE: begin
-                if (data_ready) begin
+                if (mem_ready) begin
                     next_state = COMPARE;
                     if (proc_write)
                         proc_stall = 1;
                     else
                         proc_stall = 0;
+                    proc_stall = 1;
                     
-                    mem_read                                  = 0;
+                    next_mem_read                             = 0;
                     next_cache_dm[index][`L2_DATA_LEN-1]      = 1'b0; // set dirty
                     next_cache_dm[index][`L2_DATA_LEN-2]      = 1'b1; // set valid
                     next_cache_dm[index][127+`L2_TAG_LEN:128] = tag; // set tag
@@ -439,20 +441,20 @@ module L2(
                     proc_rdata                                = mem_rdata;
                 end
                 else begin
-                    next_state = ALLOCATE;
-                    proc_stall = 1;
-                    mem_read   = 1;
+                    next_state    = ALLOCATE;
+                    proc_stall    = 1;
+                    next_mem_read = 1;
                 end
             end
             WB: begin
                 proc_stall = 1;
-                if (data_ready) begin
-                    mem_write     = 0;
+                if (mem_ready) begin
+                    next_mem_write     = 0;
                     next_mem_addr = proc_addr;
                     next_state    = ALLOCATE;
                 end
                 else begin
-                    mem_write  = 1;
+                    next_mem_write  = 1;
                     next_state = WB;
                 end
             end
@@ -467,15 +469,17 @@ module L2(
         if (proc_reset) begin
             for (j = 0; j < `L2_BLOCK_NUM; j = j + 1) cache_dm[j] <= 0;
             state      <= IDLE;
-            data_ready <= 0;
             mem_addr   <= 0;
+            mem_read   <= 0;
+            mem_write  <= 0;
             mem_wdata  <= 0;
         end
         else begin
             for (j = 0; j < `L2_BLOCK_NUM; j = j + 1) cache_dm[j] <= next_cache_dm[j];
             state      <= next_state;
-            data_ready <= mem_ready;
             mem_addr   <= next_mem_addr;
+            mem_read   <= next_mem_read;
+            mem_write  <= next_mem_write;
             mem_wdata  <= next_mem_wdata;
         end
     end
